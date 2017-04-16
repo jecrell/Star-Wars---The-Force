8b60fb4a035f70930410a22a6134da7717830055
jecrell <matt.walls31@gmail.com>
2017-04-16 19:22:58 +0900

    Added a DamageWorker for ForceChoke

---
+++ Source/ProjectJedi/DamageWorker_ForceChoke.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+using RimWorld;
+
+namespace ProjectJedi
+{
+    public class DamageWorker_ForceChoke : DamageWorker
+    {
+        public override float Apply(DamageInfo dinfo, Thing victim)
+        {
+            Pawn pawn = victim as Pawn;
+            if (pawn != null)
+            {
+                DamageInfo newDinfo = new DamageInfo(dinfo);
+                BodyPartRecord neckRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.label == "neck");
+                if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == neckRecord))
+                {
+                    newDinfo.SetForcedHitPart(neckRecord);
+                }
+                else
+                {
+                    BodyPartRecord lungRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.Activities.First<Pair<PawnCapacityDef, string>>().First == PawnCapacityDefOf.Breathing);
+                    if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == lungRecord))
+                    {
+                        newDinfo.SetForcedHitPart(lungRecord);
+                    }
+                }
+                DamageWorker_AddInjury newWorker = new DamageWorker_AddInjury();
+                return newWorker.Apply(newDinfo, victim);
+            }
+            return 0f;
+        }
+        
+    }
+}
