8d4680079c173b9804259c1283261ae50e3d27a6
jecrell <matt.walls31@gmail.com>
2017-11-11 01:45:56 +0900

    Force Choke Fix
    No longer throws errors in A18

--- Source/ProjectJedi/DamageWorker_ForceChoke.cs
+++ Source/ProjectJedi/DamageWorker_ForceChoke.cs
@@ -7,49 +7,73 @@ using RimWorld;
 
 namespace ProjectJedi
 {
-    public class DamageWorker_ForceChoke : DamageWorker
+    public class DamageWorker_ForceChoke : DamageWorker_AddInjury
     {
-        public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
+        protected override BodyPartRecord ChooseHitPart(DamageInfo dinfo, Pawn pawn)
         {
-            DamageResult result = DamageResult.MakeNew();
-            result.totalDamageDealt = 0f;
-
-            if (victim is ProjectJedi.PawnGhost)
+            BodyPartRecord rec = null;
+            BodyPartRecord neckRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.label == "neck");
+            if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == neckRecord))
             {
-                Messages.Message("PJ_ForceGhostResisted".Translate(), MessageTypeDefOf.NegativeEvent);
-                return result;
+                rec = neckRecord;
             }
-
-            if (victim is Pawn p && p.RaceProps.IsMechanoid)
+            else
             {
-                Messages.Message("PJ_ForceResisted".Translate(new object[] {
-                    p.Label.CapitalizeFirst(), dinfo.Instigator.LabelShort, dinfo.Def.label
-                }), MessageTypeDefOf.NegativeEvent);
-                return result;
-            }
-
-            Pawn pawn = victim as Pawn;
-            if (pawn != null)
-            {
-                DamageInfo newDinfo = new DamageInfo(dinfo);
-                BodyPartRecord neckRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.label == "neck");
-                if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == neckRecord))
+                BodyPartRecord lungRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.tags.First((string s) => s == "BreathingSource" || s == "BreathingPathway") != null);
+                if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == lungRecord))
                 {
-                    newDinfo.SetHitPart(neckRecord);
+                    rec = lungRecord;
                 }
-                else
-                {
-                    BodyPartRecord lungRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.tags.First((string s) => s == "BreathingSource" || s == "BreathingPathway") != null);
-                    if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == lungRecord))
-                    {
-                        newDinfo.SetHitPart(lungRecord);
-                    }
-                }
-                DamageWorker_AddInjury newWorker = new DamageWorker_AddInjury();
-                return newWorker.Apply(newDinfo, victim);
             }
-            return result;
+            return rec;
         }
-        
+
+        protected override void ApplySpecialEffectsToPart(Pawn pawn, float totalDamage, DamageInfo dinfo, ref DamageWorker.DamageResult result)
+        {
+            base.FinalizeAndAddInjury(pawn, totalDamage, dinfo, ref result);
+        }
     }
+    //public override DamageWorker.DamageResult Apply(DamageInfo dinfo, Thing victim)
+    //    {
+    //        DamageResult result = DamageResult.MakeNew();
+    //        result.totalDamageDealt = 0f;
+
+    //        if (victim is ProjectJedi.PawnGhost)
+    //        {
+    //            Messages.Message("PJ_ForceGhostResisted".Translate(), MessageTypeDefOf.NegativeEvent);
+    //            return result;
+    //        }
+
+    //        if (victim is Pawn p && p.RaceProps.IsMechanoid)
+    //        {
+    //            Messages.Message("PJ_ForceResisted".Translate(new object[] {
+    //                p.Label.CapitalizeFirst(), dinfo.Instigator.LabelShort, dinfo.Def.label
+    //            }), MessageTypeDefOf.NegativeEvent);
+    //            return result;
+    //        }
+
+    //        Pawn pawn = victim as Pawn;
+    //        if (pawn != null)
+    //        {
+    //            DamageInfo newDinfo = new DamageInfo(dinfo);
+    //            BodyPartRecord neckRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.label == "neck");
+    //            if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == neckRecord))
+    //            {
+    //                newDinfo.SetHitPart(neckRecord);
+    //            }
+    //            else
+    //            {
+    //                BodyPartRecord lungRecord = pawn.def.race.body.AllParts.FirstOrDefault((BodyPartRecord x) => x.def.tags.First((string s) => s == "BreathingSource" || s == "BreathingPathway") != null);
+    //                if (!pawn.health.hediffSet.GetHediffs<Hediff_MissingPart>().Any((Hediff_MissingPart x) => x.Part == lungRecord))
+    //                {
+    //                    newDinfo.SetHitPart(lungRecord);
+    //                }
+    //            }
+    //            DamageWorker_AddInjury newWorker = new DamageWorker_AddInjury();
+    //            return newWorker.Apply(newDinfo, victim);
+    //        }
+    //        return result;
+    //    }
+        
+    //}
 }
