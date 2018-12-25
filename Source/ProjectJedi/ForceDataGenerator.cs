77e93ed25c9d23cab26c268749d600b3f4b30135
Petru Grozea <petrugrozea@gmail.com>
2018-12-25 17:52:56 +0200

    Added a chance to spawn trained force users
    Now when a new force user is generated there is a chance that he has already begun training in the ways of the force thus adding more  variety of force users not only newbies.

---
+++ Source/ProjectJedi/ForceDataGenerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace ProjectJedi
+{
+    public static class ForceDataGenerator
+    {
+        public static ForceData RandomForceData(CompForceUser newUser)
+        {
+            ForceData forceData = new ForceData(newUser);
+
+            // are his powers discovered?
+            // the older the pawn more chance to have his powers discovered
+            if(new IntRange(1, 101).RandomInRange > forceData?.Pawn.ageTracker.AgeBiologicalYears)
+            {
+                return forceData;
+            }
+
+            // generate the level
+            int forceLevel = Mathf.Clamp(forceData.Pawn.skills.skills
+                                                  .Select(skill => skill.Level)
+                                                  .Sum() / forceData.Pawn.skills.skills.Count, 1, 50);
+            forceData.Level = forceLevel;
+
+            //select side
+            forceData.Alignment = new FloatRange(0f, 1f).RandomInRange;
+
+            while(forceLevel != 0)
+            {
+                bool upgradeSkills = new IntRange(0, 2).RandomInRange == 0 ? false : true;
+
+                if (upgradeSkills)
+                {
+                    var skill = forceData.Skills.RandomElement();
+                    if(skill?.level < 5)
+                    {
+                        skill.level++;
+                        forceLevel--;
+                    }
+                }
+                else
+                {
+                    var power = forceData.Powers.RandomElement();
+                    if(power?.level < 3)
+                    {
+                        forceData.Pawn.GetComp<CompForceUser>()?.LevelUpPower(power);
+                        forceLevel--;
+                    }
+                }
+            }
+
+            return forceData;
+        } 
+    }
+}
